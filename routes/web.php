79e4bdcf0a14cbc91416856d15ab3be5e3f54d24
McHeicho <cjpsoriano.2001@gmail.com>
2026-06-03 16:07:07 +0800

    quick fix: no 404 from user-made site jumps now.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Catch-all: hand any non-API path to the SPA so React Router can take over
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '^(?!api/).*$');
